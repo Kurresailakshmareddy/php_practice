958ae3126581cb21f47aa1a27b02f5d968584aab
kurresailakshmareddy <kurresailakshmareddy@gmail.com>
2023-09-05 11:08:19 +0100

    stringmethod

---
+++ stringmethods.php
@@ -0,0 +1,39 @@
+<?php
+   
+   //$username = "lakshma";
+   
+   //$username = strtolower($username);
+   //$username = strtoupper($username);
+   //$username = trim($username);
+   //$username = str_pad($username,3, "_");
+   //echo $username;
+
+   $username = "lakshma";
+   $phone = "123-456-7890";
+
+   /*$phone = str_replace("-","", $phone); 
+      echo "$phone";
+   //$username = strrev($username);
+   //$username = str_shuffle($username);
+   //$equals = strcmp($username,"bro code");
+   //echo "$equals";
+   //$count = strlen($phone);
+   //echo "$count";
+   //$index = strpos($phone,"-");
+   //echo "$index";
+   //$firstname = substr($username, 0 , 3);
+   //echo "$firstname";
+   //$lastname = substr($username, 4 );
+   //echo "$lastname";
+   //$fullname = explode("", $username);
+
+    foreach ($fullname as $name){
+       // echo $name . "<br>";
+    }
+
+    $username = implode ("",$username);
+    echo $username;
+    
+    */
+
+?>
