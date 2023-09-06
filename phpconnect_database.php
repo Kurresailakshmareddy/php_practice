ae34c1c2e6a636c4d745ee4eab9b9c9fe3f1658d
kurresailakshmareddy <kurresailakshmareddy@gmail.com>
2023-09-06 11:43:27 +0100

    connection

--- phpconnect_database.php
+++ phpconnect_database.php
@@ -2,22 +2,18 @@
    //1.MySQLi Extension;
    //2.PDO (PHP Data Objects)
  
-   $db_server = "localhost";
-   $db_user = "root";
-   $db_pass = "";
-   $db_name = "php";
-   $conn = "";
+  
 
-  try{
-   $conn = mysqli_connect($db_server, $db_name, $db_pass, $db_user);
- 
-  }
-  catch(mysqli_sql_exception){
-   echo "could not conenct to the server ";
-  }
+      $sname= "localhost";
+      $uname= "root";
+      $password = "";
 
-   if ($conn){
-      echo "you are connected";
-   }
+      $db_name = "php";
+
+      $conn = mysqli_connect($sname, $uname, $password, $db_name);
+
+if (!$conn) {
+	echo "Connection failed!";
+}
    
 ?>
